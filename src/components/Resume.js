e4405c3e61d572b77a623ab2770a2ac9aaaeda9f
soundariyabala <balasoundariya.ravikumar@gmail.com>
2024-06-25 17:52:51 +0530

    link 2

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -2,7 +2,7 @@ import ResumeImg from '../assets/resume.jpg';
 
  function Resume () {
     const config = {
-        link: 'https://drive.google.com/file/d/1YdsHQ5SuhoHcn86cAjmieVc1CovglPek/view?usp=drivesdk'
+        link: 'https://drive.google.com/file/d/1mrXvDdZJKS3tKWRhANUfygWMegVbInfx/view?usp=drivesdk'
     }
 
     return <section id='resume' className='flex flex-col md:flex-row bg-secondary px-5'>
@@ -12,7 +12,7 @@ import ResumeImg from '../assets/resume.jpg';
         <div className='md:w-1/2 flex justify-center'>
             <div className='flex flex-col justify-center text-white'>
                 <h1 className='text-4xl border-b-4 border-primary mb-5 w-[140px] font-bold'>Resume</h1>
-                <p className='pb-5'>You can view my resume <a className='btn' href={config.link} download> Download</a></p>
+                <p className='pb-5'>You can view my resume <a className='btn' href={config.link}> Download </a></p> 
             </div>
         </div>
     </section>
